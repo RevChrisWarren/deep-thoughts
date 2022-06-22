cc8b7ef8542b73f963756b1a421411daab4bd5f6
RevChrisWarren <chriswarren@Chriss-Mac-mini-6.local>
2022-06-22 12:24:28 -0500

    finished lesson 3

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 //import Apollo server
 const { ApolloServer } = require('apollo-server-express')
 
+const path = require('path');
+
 //import typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
@@ -26,7 +28,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   //integrate Apollo server with Express application
   server.applyMiddleware({ app });
+  //serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   db.once('open', () => {
     app.listen(PORT, () => {
